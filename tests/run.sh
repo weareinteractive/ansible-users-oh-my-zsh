ab60049853ef33e300b47420b2cebb58d6393c60
franklin <franklin@weareinteractive.com>
2015-12-08 10:37:54 +0100

    refactor: fixing users_group check

--- tests/run.sh
+++ tests/run.sh
@@ -5,6 +5,7 @@ ROLE_NAME=franklinkim.users-oh-my-zsh
 
 # install dependencies
 ansible-galaxy install franklinkim.git
+ansible-galaxy install franklinkim.users
 
 # create role symnlink
 ln -s $(pwd) /usr/share/ansible/roles/$ROLE_NAME
